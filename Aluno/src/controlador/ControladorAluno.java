ee4f5f5d8ee7d8eabcccad298a36e8fb33e5b097
Administrador <Administrador@10.10.4.4>
2019-08-06 16:50:20 -0300

    Versao1

---
+++ Aluno/src/controlador/ControladorAluno.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+import dao.DaoAluno;
+import javax.swing.JOptionPane;
+import modelo.Aluno;
+import tela.manutencao.ManutencaoAluno
+        
+public class ControladorAluno {
+    public static void inserir(ManutencaoAluno man){
+        Aluno objeto = new Aluno();
+        objeto.setNome(man.jtfNome.getText());
+        objeto.setSobrenome(man.jtfSobrenome.getText());
+        objeto.setSexo(man.jtfSexo.getText());
+        boolean resultado = DaoAluno.inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+}
+    
+}
---
+++ Aluno/src/modelo/Aluno.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Aluno {
+    private int codigo;
+    private String nome;
+    private String sobrenome;
+    private String sexo;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" + "nome=" + nome + ", sobrenome=" + sobrenome + ", sexo=" + sexo + '}';
+    }
+    
+}
